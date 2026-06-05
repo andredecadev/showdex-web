96d8a86387fb0816aaf63e7c5558e1231bf0c831
andredecadev <andredecadev@gmail.com>
2026-06-05 16:51:10 +0200

    Responsiveness fixes

--- src/components/pages/LandingPage/LandingPage.jsx
+++ src/components/pages/LandingPage/LandingPage.jsx
@@ -1,10 +1,9 @@
-import './styles/global.scss';
+import "./styles/global.scss";
 import NavWidget from "./widgets/NavWidget/NavWidget";
 import HeroWidget from "./widgets/HeroWidget/HeroWidget";
 import FeaturesWidget from "./widgets/FeaturesWidget/FeaturesWidget";
 import GalleryWidget from "./widgets/GalleryWidget/GalleryWidget";
 import FAQWidget from "./widgets/FAQWidget/FAQWidget";
-import FinalCTAWidget from "./widgets/FinalCTAWidget/FinalCTAWidget";
 import FooterWidget from "./widgets/FooterWidget/FooterWidget";
 
 const LandingPage = () => (
@@ -14,7 +13,6 @@ const LandingPage = () => (
     <FeaturesWidget />
     <GalleryWidget />
     <FAQWidget />
-    <FinalCTAWidget />
     <FooterWidget />
   </>
 );
